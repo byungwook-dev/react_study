74c5e9ce02d32d9c48847c7414a5ddf7a216ad49
byungwook-dev <byungwook.dev@gmail.com>
2024-10-31 21:50:40 +0900

    day 07

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
+import { ThemeProvider } from "styled-components";
 import "./App.css";
-import FoodContainer from "./docs/ref/expert/FoodContainer";
+import GlobalStyle from "./global/globalStyle";
+import Styled03 from "./pages/docs/_component/Styled03";
+import theme from "./global/theme";
+
 
 
 function App() {
   return (
     <>
-      <FoodContainer />
+      <ThemeProvider theme={theme}>
+        <GlobalStyle />
+        <Styled03 />
+      </ThemeProvider>
     </>
   );
 }
---
+++ src/global/common.js
@@ -0,0 +1,22 @@
+import { css } from "styled-components";
+
+export const flexCenter = css`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`
+
+export const flexCenterColumn = css`
+  ${flexCenter};
+  flex-direction: column;
+`
+
+export const flexCenterRow = css`
+  ${flexCenter};
+  flex-direction: row;
+`
+export const OrangeBox = css`
+  width: 100px;
+  height: 100px;
+  background-color: orange;
+`
